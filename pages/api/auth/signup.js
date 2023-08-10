629db08d9f2245b4cbdb12b4cfd419d4ff4b85ae
David Gerardo Martinez <gerardomartinez.hi@gmail.com>
2023-08-10 17:58:17 -0500

    sending signup requests from frontend

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,39 +1,46 @@
 import { hashPassword } from "../../../lib/auth";
 import { connectToDatabase } from "../../../lib/db";
 
-function handler(req, res) {
-  //Extract incoming data
-  const data = req.body;
+async function handler(req, res) {
+  //POST
+  if (req.method === "POST") {
+    //Extract incoming data
+    const data = req.body;
 
-  const { email, password } = data;
+    const { email, password } = data;
 
-  //Validate incoming data
-  if (
-    !email ||
-    !email.includes("@") ||
-    !password ||
-    password.trim().length < 7
-  ) {
-    res.status(422).json({
-      message: "Invalid input - password should be at least 7 characters long",
+    //Validate incoming data
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
+        message:
+          'Invalid input - password should also be at least 7 characters long.',
+      });
+      return;
+    }
+
+    //connect to database
+    const client = await connectToDatabase();
+
+    const db = client.db();  
+
+    //hash password
+    const hashedPassword = await hashPassword(password);
+
+    const result = await db.collection("users").insertOne({
+      email: email,
+      password: hashedPassword,
     });
-    return;
+
+    res.status(201).json({ message: "Created user!" });
+    client.close();
+
   }
-
-  //connect to database
-  const client = connectToDatabase();
-
-  const db = client.db();
-
-  //hash password
-  const hashedPassword = hashPassword(password);
-
-  db.collection("users").insertOne({
-    email: email,
-    password: hashedPassword,
-  });
-
-  res.status(201).json({ message: "Created user!" });
 }
 
 export default handler;
